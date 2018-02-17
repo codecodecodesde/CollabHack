b190015619ce6d2f0ff5573bae2526ab67d6a2aa
misspink1011 <xuanwang1118@gmail.com>
2018-02-17 16:15:14 -0500

    build client as static file

--- oj-server/server.js
+++ oj-server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const http = require('http');
 const restRouter = require('./routes/rest');
+
 
 //connect to mongoDB
 const mongoose = require('mongoose');
@@ -8,4 +11,11 @@ mongoose.connect('mongodb://erinbiubiubiu:summerday.123@ds239368.mlab.com:39368/
 
 //app.get('/', (req, res) => res.send('Hello World!!'));
 app.use('/api/v1', restRouter);
+app.use(express.static(path.join(__dirname, '../public')));
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+//url not handled by router on the server side, then server send index.html under public folder
+app.use((req, res) => {
+    res.sendFile('index.html', { root: path.join(__dirname, '../public')});
+});
