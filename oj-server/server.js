a0d99506e6dce4777dbf17b525999019124adcdd
misspink1011 <xuanwang1118@gmail.com>
2018-02-17 11:36:05 -0500

    implement restful api and connect to mongoDB

--- oj-server/server.js
+++ oj-server/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const restRouter = require('./routes/rest');
 
+//connect to mongoDB
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://erinbiubiubiu:summerday.123@ds239368.mlab.com:39368/testv1');
+
 //app.get('/', (req, res) => res.send('Hello World!!'));
 app.use('/api/v1', restRouter);
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
