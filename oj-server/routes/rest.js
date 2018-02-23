87782796a0e5072f38c6ed68a396e1229314ad4c
misspink1011 <xuanwang1118@gmail.com>
2018-02-23 18:25:52 -0500

    connect to Flask web server

--- oj-server/routes/rest.js
+++ oj-server/routes/rest.js
@@ -5,6 +5,11 @@ const problemService = require('../services/problemService');
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
+const nodeRestClient = require('node-rest-client').Client;
+const restClient = new nodeRestClient();
+EXECUTOR_SERVER_URL = 'http://localhost:5000/build_and_run';
+restClient.registerMethod('build_and_run', EXECUTOR_SERVER_URL, 'POST')
+
 //get all problems
 router.get('/problems', (req, res) => {
   problemService.getProblems()
@@ -23,6 +28,24 @@ router.post('/problems', jsonParser, (req, res) => {
   problemService.addProblem(req.body)
     .then(problems => res.json(problems),
           error => res.status(400).send("Problem name already exists"));
+});
+
+router.post('/build_and_run', jsonParser, (req, res) => {
+  const userCode = req.body.user_code;
+  const lang = req.body.lang;
+  console.log('lang:', lang, 'code:', userCode);
+
+  restClient.methods.build_and_run(
+  {
+    data: {code: userCode, lang: lang},
+    headers: {'Content-Type': 'application/json'}
+  },
+  (data, response) => {
+    const text = `Build output: ${data['build']}, , execute output:
+    ${data['run']}`;
+    console.log(text);
+    res.json(text);
+  })
 });
 
 module.exports = router;
